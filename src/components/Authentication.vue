6b8fe71082a97df2b43d0dc1c1fe6d880ccdf420
tokmashik <tokmashik2@gmail.com>
2022-06-24 09:47:08 +0700

    commit

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,5 +1,4 @@
 <template>
-
     <v-dialog
       v-model="dialog"
       persistent
---
+++ src/table.js
@@ -0,0 +1,27 @@
+export const TableTS = [
+  {
+    id: 1,
+    transport: '32323',
+    cargo: 'metal'
+  },
+  {
+    id: 2,
+    transport: '32324',
+    cargo: 'lom'
+  },
+  {
+    id: 3,
+    transport: '32325',
+    cargo: 'lom'
+  },
+  {
+    id: 4,
+    transport: '32326',
+    cargo: 'metal'
+  },
+  {
+    id: 5,
+    transport: '32327',
+    cargo: 'lom'
+  },
+]
